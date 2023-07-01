ae97fe02f6688327357111dca5a601bdb8cb451a
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-01 10:28:19 +0200

    feat: generating random number
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,7 +1,10 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     println!("Please input your guess.");
 
