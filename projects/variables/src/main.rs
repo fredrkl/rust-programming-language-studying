ed9b9518159f91cdb3622e7cbff4fcb8555cdb34
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-04 23:40:20 +0200

    feat: Introducing const
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -3,4 +3,6 @@ fn main() {
     println!("The value of x is: {x}");
     x = 6;
     println!("The value of x is: {x}");
+    const THREE_HOURS_IN_SECONDS: u16 = 60 * 60 * 3;
+    println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
 }
