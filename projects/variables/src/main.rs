12e8f2f271364b9fcc719c72e26fbc1d44be8608
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-05 10:04:25 +0200

    feat: Inner scope variable demo
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -2,6 +2,12 @@ fn main() {
     let mut x = 5;
     println!("The value of x is: {x}");
     x = 6;
+
+    {
+        let x = x + 1;
+        println!("The value of x inner-scope is: {x}");
+    }
+
     println!("The value of x is: {x}");
     const THREE_HOURS_IN_SECONDS: u16 = 60 * 60 * 3;
     println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
