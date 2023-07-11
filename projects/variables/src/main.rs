5d9b4313400fd948f88609f79ec4933e94e25c24
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-11 21:50:46 +0200

    feat: floating point scalar types
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -14,4 +14,14 @@ fn main() {
     
     const THREE_HOURS_IN_SECONDS: u16 = 60 * 60 * 3;
     println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
+
+    let _x : u16 = 1_000;
+    let _mybyte : u8 = 0b1111_0000;
+    println!("The value of _mybyte is now: {_mybyte}");
+
+    // Floating point types
+    let _x = 2.0; // f64
+    let _y: f32 = 3.0; // f32
+    println!("The value of _x is now: {_x}");
+    println!("The value of _y is now: {_y}");
 }
