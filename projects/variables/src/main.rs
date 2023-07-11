b5091b1485fdf3a8d710ff6b8cfc7c3a5398751f
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-11 21:21:06 +0200

    ref: cleanup
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,15 +1,17 @@
 fn main() {
     let mut x = 5;
     let y = 5;
+    println!("The value of y is: {y}");
     println!("The value of x is: {x}");
-    x = 6;
 
+    x = 6;
     {
         let y = y + 1;
         println!("The value of y inner-scope is: {y}");
     }
 
-    println!("The value of x is: {x}");
+    println!("The value of x is now: {x}");
+    
     const THREE_HOURS_IN_SECONDS: u16 = 60 * 60 * 3;
     println!("The value of THREE_HOURS_IN_SECONDS is: {THREE_HOURS_IN_SECONDS}");
 }
