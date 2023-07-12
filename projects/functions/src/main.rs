685206fff33269addb480d476b582bc3c4cdf15f
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-12 21:04:02 +0200

    feat: functions with parameters
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -2,8 +2,18 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    another_function_with_parameter(5);
+    
 }
 
 fn another_function(){
     println!("Another function");
+}
+
+fn another_function_with_parameter(x: i32){
+    println!("Another function with parameter. The argument of x is: {}", x);
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char){
+    println!("The value is: {} {}", value, unit_label);
 }
