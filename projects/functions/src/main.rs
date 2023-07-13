446e572f377207e42c3a0564cfe4a2df7c1f4725
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-13 10:30:30 +0200

    feat: function return value
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     another_function();
     another_function_with_parameter(5);
     print_labeled_measurement(5, 'm'); 
+    println!("The value of five is: {}", five());
+    println!("The value of plus_one(5) is: {}", plus_one(5));
 }
 
 fn another_function(){
@@ -16,4 +18,12 @@ fn another_function_with_parameter(x: i32){
 
 fn print_labeled_measurement(value: i32, unit_label: char){
     println!("The value is: {} {}", value, unit_label);
+}
+
+fn five () -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
