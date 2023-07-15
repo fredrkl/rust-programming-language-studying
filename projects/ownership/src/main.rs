7f177364c70068d465144a89098895fd653c198e
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-15 13:04:58 +0200

    feat: ownership demo
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -9,8 +9,25 @@ fn main() {
     println!("{}", s); // This works because s is cloned into the takes_ownership method
     takes_ownership(s);
     // println!("{}", s); // This doesn't work because s is moved into the takes_ownership method
+
+    let x = 5;
+    takes_ownership_int(x);
+    println!("{}", x); // This works because i32 is a Copy type
+
+    let x = String::from("hello");
+    let x2 = takes_ownership_and_gives_back(x);
+    println!("{}", x2); // This works because x is returned from the takes_ownership_and_gives_back method
+}
+
+fn takes_ownership_and_gives_back(some_string: String) -> String {
+    println!("{}", some_string);
+    some_string
 }
 
 fn takes_ownership(some_string: String) {
     println!("{}", some_string);
+}
+
+fn takes_ownership_int(some_int: i32) {
+    println!("{}", some_int);
 }
