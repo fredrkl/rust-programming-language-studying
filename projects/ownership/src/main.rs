dd3a7ddd2a1270b74c5b43babe76b2858db97aaa
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-15 12:42:59 +0200

    feat: ownership demo
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -1,5 +1,16 @@
 fn main() {
-    let mut s = String::from("hello");
-    s.push_str(", world!");
-    println!("{}", s)
+    let s1 = String::from("hello");
+    let s2 = s1;
+    // s1.push_str(", world!");
+    println!("{}", s2);
+
+    let s = String::from("hello");
+    takes_ownership(s.clone());
+    println!("{}", s); // This works because s is cloned into the takes_ownership method
+    takes_ownership(s);
+    // println!("{}", s); // This doesn't work because s is moved into the takes_ownership method
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
 }
