1258cc45364143c875dcc6723e0e6e74d89a4af3
Fredrik Klingenberg <fredrkl@gmail.com>
2023-07-17 21:08:16 +0200

    ref: first_word now takes string slice
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/ownership/src/main.rs
+++ projects/ownership/src/main.rs
@@ -40,9 +40,13 @@ fn main() {
     let word = first_word(&s);
     println!("{}", word);
 
+    let my_string = String::from("hello world");
+    let word = first_word(&my_string);
+    println!("{}", word);
+
 }
 
-fn first_word(s: &String) -> &str {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
