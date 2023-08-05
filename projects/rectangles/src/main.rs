81406859142340504929917fff1abb5c752beb21
Fredrik Klingenberg <fredrkl@gmail.com>
2023-08-05 23:19:48 +0200

    feat: Using structs for clarity
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,13 +1,20 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let width1 = 30;
-    let height1 = 50;
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width1, height1)
+        area(&rect1)
     );
 }
 
-fn area(width: u32, length: u32) -> u32 {
-    width * length
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
