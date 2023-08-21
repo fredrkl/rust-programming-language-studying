2ab8defff9e644e820b8ba4fea5e863a3f6f7f10
Fredrik Klingenberg <fredrkl@gmail.com>
2023-08-21 20:31:17 +0200

    feat: debug trait example
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -5,11 +5,13 @@ struct Rectangle {
 }
 
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
+    dbg!(&rect1);
     println!("rect1 is {:#?}", rect1);
     println!("The area of the rectangle is {} square pixels.", area(&rect1));
 }
