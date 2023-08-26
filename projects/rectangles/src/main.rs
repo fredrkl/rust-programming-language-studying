22b0a3d1969b4d1a3fba80edb7246c2eb53ae6c8
Fredrik Klingenberg <fredrkl@gmail.com>
2023-08-26 21:41:29 +0200

    feat: method instead of function
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -4,6 +4,17 @@ struct Rectangle {
     height: u32,
 }
 
+// Implementing the area method on the Rectangle struct
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
 fn main() {
     let scale = 2;
     let rect1 = Rectangle {
@@ -12,10 +23,12 @@ fn main() {
     };
 
     dbg!(&rect1);
-    println!("rect1 is {:#?}", rect1);
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
+    //println!("rect1 is {:#?}", rect1);
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
+    println!("The width of the rectangle is larger than 0 {}: ", rect1.width());
 }
 
+/*
 fn area(rectangle: &Rectangle) -> u32 {
     rectangle.width * rectangle.height
-}
+}*/
