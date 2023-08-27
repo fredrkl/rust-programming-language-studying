78930bed347da2cf4fc9fcb22d2f88f3026de9be
Fredrik Klingenberg <fredrkl@gmail.com>
2023-08-27 16:41:18 +0200

    fix: remove unwanted code
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/enum-pattern-matching/src/main.rs
+++ projects/enum-pattern-matching/src/main.rs
@@ -1,17 +1,11 @@
-enum IpAddrKind {
-    V4(String),
+enum IpAddr {
+    V4(u8, u8, u8, u8),
     V6(String),
 }
 
 fn main() {
+    let _home = IpAddr::V4(127,0,0,1);
+    let _loopback = IpAddr::V6(String::from("::1"));
     println!("Hello, world again!");
-    let _home = IpAddrKind::V4(String::from("127.0.0.1"));
-    let _loopback = IpAddrKind::V6(String::from("::1"));
     // Rust uses :: instead of . to access the enum variants and differentiate it from field access or method calls.
-    //route(four);
-    //route(six);
 }
-
-/*fn route(ip_kind: IpAddrKind) {
-    println!("ip_kind: {:?}", ip_kind);
-}*/
