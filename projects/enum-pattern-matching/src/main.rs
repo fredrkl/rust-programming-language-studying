8bb467c760d5758a347fb8c318406976795513b5
Fredrik Klingenberg <fredrkl@gmail.com>
2023-08-27 16:29:25 +0200

    feat: IP address enum
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/enum-pattern-matching/src/main.rs
+++ projects/enum-pattern-matching/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+enum IpAddrKind {
+    V4,
+    V6,
 }
+
+fn main() {
+    println!("Hello, world again!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    // Rust uses :: instead of . to access the enum variants and differentiate it from field access or method calls.
+    //route(four);
+    //route(six);
+}
+
+/*fn route(ip_kind: IpAddrKind) {
+    println!("ip_kind: {:?}", ip_kind);
+}*/
