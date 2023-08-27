2d5d88e33d5871dc3dd099ee72b3226eb38842d8
Fredrik Klingenberg <fredrkl@gmail.com>
2023-08-27 16:35:18 +0200

    feat: add data to enum
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/enum-pattern-matching/src/main.rs
+++ projects/enum-pattern-matching/src/main.rs
@@ -1,12 +1,12 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(String),
+    V6(String),
 }
 
 fn main() {
     println!("Hello, world again!");
-    let four = IpAddrKind::V4;
-    let six = IpAddrKind::V6;
+    let _home = IpAddrKind::V4(String::from("127.0.0.1"));
+    let _loopback = IpAddrKind::V6(String::from("::1"));
     // Rust uses :: instead of . to access the enum variants and differentiate it from field access or method calls.
     //route(four);
     //route(six);
