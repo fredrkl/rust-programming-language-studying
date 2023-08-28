c0942d8ecdf9468310ed9455e540f27b5087d6be
Fredrik Klingenberg <fredrkl@gmail.com>
2023-08-28 19:58:14 +0200

    feat: enum and structs
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/enum-pattern-matching/src/main.rs
+++ projects/enum-pattern-matching/src/main.rs
@@ -3,9 +3,27 @@ enum IpAddr {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move {x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32), 
+}
+
+impl Message {
+   fn call(&self){
+    println!("Now we are printing out");
+   } 
+}
+
 fn main() {
     let _home = IpAddr::V4(127,0,0,1);
     let _loopback = IpAddr::V6(String::from("::1"));
     println!("Hello, world again!");
+
+    let _something = Message::Move { x: 34, y: 45 };
+    let _somethingelse = Message::Write(String::from("Hello, world!"));
+
+    _somethingelse.call();
     // Rust uses :: instead of . to access the enum variants and differentiate it from field access or method calls.
 }
