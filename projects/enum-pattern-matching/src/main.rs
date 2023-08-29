7bbbf5937b14589ef2804c72d93d625d5cc07cc1
Fredrik Klingenberg <fredrkl@gmail.com>
2023-08-29 20:51:10 +0200

    feat: showcasing enum as value for enum
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/enum-pattern-matching/src/main.rs
+++ projects/enum-pattern-matching/src/main.rs
@@ -16,11 +16,18 @@ impl Message {
    } 
 }
 
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska,
+    // --snip--
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState), // enum inside an enum
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -31,7 +38,10 @@ fn value_in_cents(coin: Coin) -> u8 {
         },
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
