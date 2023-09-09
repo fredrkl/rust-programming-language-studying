d7b05147cfd0cd3b3965fd45a39c25bb03070c1c
Fredrik Klingenberg <fredrkl@gmail.com>
2023-09-09 09:49:54 +0200

    doc: library vs executable
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/my-project/src/main.rs
+++ projects/my-project/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// By convention, the main function is in a file named main.rs or src/main.rs in the project root. If we create a library then the convention is to have a src/lib.rs file in the project root.
