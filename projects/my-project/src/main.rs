d5fc82bf96b3591edb11dfe4133cb47bb142702a
Fredrik Klingenberg <fredrkl@gmail.com>
2023-09-09 10:30:30 +0200

    feat: modules example
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/my-project/src/main.rs
+++ projects/my-project/src/main.rs
@@ -3,3 +3,4 @@ fn main() {
 }
 
 // By convention, the main function is in a file named main.rs or src/main.rs in the project root. If we create a library then the convention is to have a src/lib.rs file in the project root.
+// Use https://doc.rust-lang.org/stable/book/ch07-02-defining-modules-to-control-scope-and-privacy.html as a reference for modules and visibility.
