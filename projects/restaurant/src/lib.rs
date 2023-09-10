469ae0adaa19bd84fb2b87a059ff76ce54c703bc
Fredrik Klingenberg <fredrkl@gmail.com>
2023-09-10 21:31:20 +0200

    feat: back of the house module
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,13 +1,48 @@
-mod front_of_house {
+pub mod front_of_house {
     pub mod hosting {
-        fn add_to_waitlist() {}
+        pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
-
     mod serving {
         fn take_order() {}
         fn serve_order() {}
         fn take_payment() {}
     }
 }
+
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fuit: String
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fuit: String::from("peaches")
+            }
+        }
+    }
+
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order(); // super is like .. in file system
+    }
+    fn cook_order() {}
+}
+
+fn deliver_order() {}
+
+pub fn eat_at_resturant(){
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+
+
+    crate::front_of_house::hosting::add_to_waitlist(); // Absolute path
+    front_of_house::hosting::add_to_waitlist(); // Relative path
+}
+
 // Crate is the root module of the crate tree
