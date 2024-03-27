473ea5c511cff7e880bfea956b3a3823d60761a5
Fredrik Klingenberg <fredrkl@gmail.com>
2024-03-27 16:16:28 +0100

    feat: hash-map init
    Signed-off-by: Fredrik Klingenberg <fredrkl@gmail.com>

---
+++ projects/hash-map/src/main.rs
@@ -0,0 +1,8 @@
+use std::collections::HashMap;
+
+fn main() {
+    println!("Hello, world!");
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+}
